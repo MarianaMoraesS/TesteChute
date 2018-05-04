99d18f101828d7acb00d1c8cfe244d19ca02ac21
Mariana Moraes <38966028+MarianaMoraesS@users.noreply.github.com>
2018-05-03 23:03:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// Meu
+
+let senha = 12;
+let chute = prompt("Adivinhe a senha de dois digitos entre 10 e 20:");
+let stop = alert("Tente de Novo");
+
+while (chute != senha){
+    chute = prompt("Adivinhe a senha de dois digitos entre 10 e 20:");
+}if (chute = senha){
+    alert("Acertou");
+} else {
+    stop = alert("Tente de Novo");
+}
+
+// Professor
+
+// let senha = "1234";
+// let resposta = prompt ("Digite a senha:")
+
+// while (reposta != senha){
+//     resposta = prompt("Tente Novamente:"); //só isso fica num looping infinito
+// }
+
+// alert("Acertou")
